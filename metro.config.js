a39c89ccdb723c7a5327b103d1bb2f780132f4b3
Sealshusky <129021061+Sealshusky@users.noreply.github.com>
2026-03-24 16:18:52 -0700

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -5,15 +5,16 @@ const config = getDefaultConfig(__dirname);
 config.transformer = {
   ...config.transformer,
   babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  svgo: false,
 };
 
 config.resolver = {
   ...config.resolver,
   assetExts: config.resolver.assetExts
     .filter(ext => ext !== 'svg')
-    .concat(['sql']),
+    .concat(['wasm', 'sql']),
   sourceExts: config.resolver.sourceExts
-    .filter(ext => ext !== 'sql')
+    .filter(ext => ext !== 'wasm' && ext !== 'sql')
     .concat(['svg']),
 };
 
