b6be98a329758e5d6f2c89cc8009b7421a2a5638
dallasWed <dallasweddle95@gmail.com>
2026-03-23 23:02:04 -0700

    Added purchase button, need to implement dev points

--- src/app/(tabs)/dice-shop.jsx
+++ src/app/(tabs)/dice-shop.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import DiceView from '../../components/textured-die';
+import PurchaseButton from '../../components/purchase-button';
+import { getDB } from '@/db/db';
 
 export default function ShopScreen() {
   return (
     <View style={styles.container}>
-      <DiceView setId={1} />
+      <DiceView setId={2} />
+      <PurchaseButton
+        setId={2}
+        price={20}
+        owned={false}
+        onPurchaseSuccess={() => console.log('Purchase Complete')}
+      />
     </View>
   );
 }
