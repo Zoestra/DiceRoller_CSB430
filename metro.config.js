5fbe4ef774085abadef6e8d796dfde0cac2b2f38
Sealshusky <129021061+Sealshusky@users.noreply.github.com>
2026-03-24 05:10:56 -0700

    Refactor metro.config.js for asset and source extensions
    Remove SVG transformer configuration and update asset and source extensions.

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-
 const config = getDefaultConfig(__dirname);
-
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
-};
-
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
-  sourceExts: [...config.resolver.sourceExts, 'svg'],
-};
-
+config.resolver.assetExts = Array.from(new Set([...config.resolver.assetExts, 'wasm', 'sql']));
+config.resolver.sourceExts = config.resolver.sourceExts.filter((extension) => extension !== 'wasm' && extension !== 'sql');
 module.exports = config;
