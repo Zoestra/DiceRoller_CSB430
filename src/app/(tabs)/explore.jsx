ec7cd3a5cf021c04145e3c9fd0473b2050e2df15
Sealshusky <129021061+Sealshusky@users.noreply.github.com>
2026-03-23 19:30:17 -0700

    Use theme from settings for header background color

--- src/app/(tabs)/explore.jsx
+++ src/app/(tabs)/explore.jsx
@@ -8,11 +8,14 @@ import { ThemedView } from '@/components/themed-view';
 import { Collapsible } from '@/components/ui/collapsible';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
+import { useSettings } from '@/context/SettingsContext';
 
 export default function TabTwoScreen() {
+  const { theme } = useSettings();
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={theme === 'dark' ? '#353636' : '#D0D0D0'}
       headerImage={
         <IconSymbol
           size={310}
