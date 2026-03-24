d8d84fc8366a1a9f22f48c1d1dbedb76551b1d05
Turan Turkmen <fireflame323@hotmail.com>
2026-03-24 16:59:56 -0700

    fix metroconfig

--- metro.config.js
+++ metro.config.js
@@ -1,27 +1,19 @@
 const { getDefaultConfig } = require('expo/metro-config');
-<<<<<<< HEAD
 
-const config = getDefaultConfig(__dirname);
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  svgo: false,
-};
+  config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    svgo: false,
+  };
 
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts
-    .filter(ext => ext !== 'svg')
-    .concat(['wasm', 'sql']),
-  sourceExts: config.resolver.sourceExts
-    .filter(ext => ext !== 'wasm' && ext !== 'sql')
-    .concat(['svg']),
-};
+  config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...config.resolver.sourceExts, 'svg'],
+  };
 
-=======
-const config = getDefaultConfig(__dirname);
-config.resolver.assetExts = Array.from(new Set([...config.resolver.assetExts, 'wasm', 'sql']));
-config.resolver.sourceExts = config.resolver.sourceExts.filter((extension) => extension !== 'wasm' && extension !== 'sql');
->>>>>>> f7a6a365c511fc3d8eb7aefe66c1d12a07cd9610
-module.exports = config;
+  return config;
+})();
