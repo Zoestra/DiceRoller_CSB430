cf2d92dfa57e3023382793c940bc4fce66438897
Sealshusky <129021061+Sealshusky@users.noreply.github.com>
2026-03-23 22:08:27 -0700

    Update import paths in explore.jsx

--- src/app/(tabs)/explore.jsx
+++ src/app/(tabs)/explore.jsx
@@ -1,14 +1,14 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Collapsible } from '@/components/ui/collapsible';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
-import { useSettings } from '@/context/SettingsContext';
+import { ExternalLink } from '../../components/external-link';
+import ParallaxScrollView from '../../components/parallax-scroll-view';
+import { ThemedText } from '../../components/themed-text';
+import { ThemedView } from '../../components/themed-view';
+import { Collapsible } from '../../components/ui/collapsible';
+import { IconSymbol } from '../../components/ui/icon-symbol';
+import { Fonts } from '../../constants/theme';
+import { useSettings } from '../../hooks/SettingsContext';
 
 export default function TabTwoScreen() {
   const { theme } = useSettings();
